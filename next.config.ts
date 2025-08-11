5fb8384328fd6f24634d8d83e0e39763a0e30052
Wahyu Dwi Nugraha <nugrahawd9@gmail.com>
2025-08-11 21:45:28 +0700

    Testing next config basepath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/portfolio' : '',
+  basePath: '/portfolio',
   output: 'export',
   images: {
     unoptimized: true,
