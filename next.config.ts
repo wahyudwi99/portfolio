b994faddc2dd7960c6381142be2d83c3ee6e4e1b
Wahyu Dwi Nugraha <nugrahawd9@gmail.com>
2025-08-11 19:44:53 +0700

    Fixing package issues

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  basePath: isProd ?  '/https://wahyudwi99.github.io/portfolio/' : '',
-  assetPrefix: isProd ? '/https://wahyudwi99.github.io/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
   output: 'export',
   images: {
     unoptimized: true,
